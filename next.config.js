3afcbc70ee432b14f3067ef3e4e8b4c090dd7e5e
javalit <javalit@coventry.ac.uk>
2025-07-16 15:45:52 +0100

    new commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  // Allow WebSocket connections
-  async rewrites() {
-    return [
-      {
-        source: "/api/ws",
-        destination: "ws://localhost:8080",
-      },
-    ]
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
